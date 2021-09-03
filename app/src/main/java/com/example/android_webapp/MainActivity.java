93de74c2d2889a224ee76eb9db51d44b11b6ccdc
shlomi <shl.lev.ks@gmail.com>
2021-09-03 14:39:40 +0300

    connect items to code, add volly(http package)

--- app/src/main/java/com/example/android_webapp/MainActivity.java
+++ app/src/main/java/com/example/android_webapp/MainActivity.java
@@ -3,12 +3,56 @@ package com.example.android_webapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText ed_dataInput;
+    ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign value to each control on the layout
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+        ed_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReport);
+
+        // click listeners for each button
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toastMessage("btn city click");
+            }
+        });
+
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toastMessage("get w by id");
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toastMessage("get w by name");
+            }
+        });
+
+
+    }
+
+    private void toastMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }

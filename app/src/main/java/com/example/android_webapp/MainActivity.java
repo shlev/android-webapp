436bf99e1a0efbbc8e87db01fb47e0e8d4c0d397
shlomi <shl.lev.ks@gmail.com>
2021-09-03 17:07:23 +0300

    permit internet, implements api req

--- app/src/main/java/com/example/android_webapp/MainActivity.java
+++ app/src/main/java/com/example/android_webapp/MainActivity.java
@@ -9,10 +9,23 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
-    EditText ed_dataInput;
+    EditText et_dataInput;
     ListView lv_weatherReport;
 
     @Override
@@ -24,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         btn_cityID = findViewById(R.id.btn_getCityID);
         btn_getWeatherByID = findViewById(R.id.btn_getWeatherCityID);
         btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
-        ed_dataInput = findViewById(R.id.et_dataInput);
+        et_dataInput = findViewById(R.id.et_dataInput);
         lv_weatherReport = findViewById(R.id.lv_weatherReport);
 
         // click listeners for each button
@@ -32,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 toastMessage("btn city click");
+                callApi();
             }
         });
 
@@ -54,5 +68,47 @@ public class MainActivity extends AppCompatActivity {
 
     private void toastMessage(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void callApi () {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String queryInput = et_dataInput.getText().toString();
+        String url ="https://www.metaweather.com/api/location/search/?query=" + queryInput;
+
+// Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Display the first 500 characters of the response string.
+//                        toastMessage("Response is: "+ response);
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                toastMessage("Error Occurred");
+//            }
+//        });
+        JsonArrayRequest  request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                String cityID = "";
+                try {
+                    JSONObject cityInfo = response.getJSONObject(0);
+                    cityID = cityInfo.getString("woeid");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                toastMessage("City ID = " + cityID);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                toastMessage("Something Wrong");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(request);
     }
 }
